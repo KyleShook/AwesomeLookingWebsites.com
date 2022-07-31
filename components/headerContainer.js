3cc989d259d97f17a3adb03bb446754c8eea34b3
Kyle Shook <okshook@gmail.com>
2022-07-31 13:43:30 -0500

    404

--- components/headerContainer.js
+++ components/headerContainer.js
@@ -1,6 +1,6 @@
 export default function HeaderContainer({ children }) {
 	return (
-		<div className="xl:container md:mx-auto sm:mx-3 px-5">{children}</div>
+		<div className="xl:container md:mx-auto sm:mx-3 md:px-5">{children}</div>
 		// Sticky Container
 		// <div className="xl:container md:mx-auto sm:mx-3 px-5 sticky top-0 h-10 z-10">
 		// 	{children}
---
+++ pages/404.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import Container from "../components/container";
+import Link from "next/link";
+
+export default function Custom404() {
+	const Text = styled.h1`
+		font-size: 150px;
+		font-weight: 900;
+		text-transform: uppercase;
+		text-align: center;
+		line-height: 1;
+		mix-blend-mode: difference;
+
+		@media (max-width: 1260px) {
+			font-size: 150px;
+		}
+		@media (max-width: 1000px) {
+			font-size: 100px;
+		}
+		@media (max-width: 675px) {
+			font-size: 55px;
+		}
+	`;
+
+	const Home = styled.a`
+		font-size: 50px;
+		font-weight: 900;
+		text-transform: uppercase;
+		mix-blend-mode: difference;
+		display: flex;
+		justify-content: center;
+		margin-top: 40px;
+		text-decoration: underline;
+		cursor: pointer;
+		transition: background 0.3s;
+
+		:hover {
+			background: #b9ffa6d4;
+		}
+
+		@media (max-width: 1260px) {
+			font-size: 150px;
+		}
+		@media (max-width: 1000px) {
+			font-size: 100px;
+		}
+		@media (max-width: 675px) {
+			font-size: 55px;
+		}
+	`;
+
+	return (
+		<Container>
+			<Text>TOTALLY NOT AWESOME</Text>
+			<Link href="/">
+				<Home>Let's go home.</Home>
+			</Link>
+		</Container>
+	);
+}
